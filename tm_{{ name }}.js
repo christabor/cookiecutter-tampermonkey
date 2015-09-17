3fe41937374e4d33c0edee36d06784e7af7fa8b7
Chris Tabor <dxdstudio@gmail.com>
2015-09-17 13:47:57 -0700

    Add file

---
+++ tm_{{ name }}.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name {{ name }}
+// @namespace {{ namespace }}
+// @version {{ version }}
+// @author {{ author }}
+// @contributor {{ contributor }}
+// @description {{ description }}
+// @homepageURL {{ homepageURL }}
+// @website {{ website }}
+// @source {{ source }}
+// @iconURL {{ iconURL }}
+// @icon64URL {{ icon64URL }}
+// updateURL {{ updateURL }}
+// @downloadURL {{ downloadURL }}
+// @supportURL {{ supportURL }}
+// @include {{ include }}
+// @match {{ match }}
+// @exclude {{ exclude }}
+// @require {{ require }}
+// @resource {{ resource }}
+// @run-at {{ run_at }}
+// @grant {{ grant }}
+// @user-agent {{ user_agent }}
+// @noframes {{ noframes }}
+// @unwrap {{ unwrap }}
+// @nocompat {{ nocompat }}
+// ==/UserScript==
+
+// Documentation here: http://tampermonkey.net/documentation.php
+// Put all your JS below!
